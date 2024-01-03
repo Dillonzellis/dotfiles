94ea5c0fb958e98a66e15272849d3e998b5edc6e
dillonzellis <dillonzellis@gmail.com>
2024-01-02 23:40:54 -0500

    modify null-ls to format on save

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,15 +1,22 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.eslint_d,
-			},
-		})
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.eslint_d,
+      },
+    })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+
+    -- Set up auto command to format on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      callback = function()
+        vim.lsp.buf.format({ async = false })
+      end,
+    })
+  end,
 }
