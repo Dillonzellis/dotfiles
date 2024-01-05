48f3908aa33899c54dc32c8d646032d29df2122e
dillonzellis <dillonzellis@gmail.com>
2024-01-04 21:07:31 -0500

    Fix: update harpoon keymaps

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,41 +1,16 @@
 return {
-	"ThePrimeagen/harpoon",
-	branch = "harpoon2",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	config = function()
-		local harpoon = require("harpoon")
-		-- vim.keymap.set("n", "<leader>hm", "<cmd>lua require('harpoon.mark').add_file()<cr>")
-		-- vim.keymap.set("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>")
-		-- vim.keymap.set("n", "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>")
-
-		vim.keymap.set("n", "<leader>a", function()
-			harpoon:list():append()
-		end)
-		vim.keymap.set("n", "<C-e>", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end)
-
-		vim.keymap.set("n", "<C-h>", function()
-			harpoon:list():select(1)
-		end)
-		vim.keymap.set("n", "<C-t>", function()
-			harpoon:list():select(2)
-		end)
-		vim.keymap.set("n", "<C-n>", function()
-			harpoon:list():select(3)
-		end)
-		vim.keymap.set("n", "<C-s>", function()
-			harpoon:list():select(4)
-		end)
-
-		-- Toggle previous & next buffers stored within Harpoon list
-		vim.keymap.set("n", "<C-S-P>", function()
-			harpoon:list():prev()
-		end)
-		vim.keymap.set("n", "<C-S-N>", function()
-			harpoon:list():next()
-		end)
-	end,
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    local harpoon = require("harpoon")
+    vim.keymap.set("n", "<leader>a", function()
+      harpoon:list():append()
+    end)
+    vim.keymap.set("n", "<C-e>", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
+  end,
 }
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,18 +1,18 @@
 return {
-	"christoomey/vim-tmux-navigator",
-	"github/copilot.vim",
+  "christoomey/vim-tmux-navigator",
+  "github/copilot.vim",
 
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-	},
-	{
-		"kdheepak/lazygit.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		config = function()
-			vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>")
-		end,
-	},
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>")
+    end,
+  },
 }
