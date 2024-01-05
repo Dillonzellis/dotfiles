a932c3a21c9dc5eda9163e2de4f94c57d8ad6342
dillonzellis <dillonzellis@gmail.com>
2024-01-04 20:13:17 -0500

    fix: move simple plugins to init file

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,18 @@
+return {
+	"christoomey/vim-tmux-navigator",
+	"github/copilot.vim",
+
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>")
+		end,
+	},
+}
