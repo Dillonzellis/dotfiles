c32dab5c668818d329864de05fa418254269deaa
dillonzellis <dillonzellis@gmail.com>
2024-01-07 09:25:41 -0500

    fix: remove duplicate copilot

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,25 +1,18 @@
 return {
-  "christoomey/vim-tmux-navigator",
-  "github/copilot.vim",
-
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    opts = {},
-  },
-  {
-    "kdheepak/lazygit.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>")
-    end,
-  },
-  {
-    "github/copilot.vim",
-    config = function()
-      vim.api.nvim_set_keymap("i", "<CR>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
-    end,
-  },
+	"christoomey/vim-tmux-navigator",
+	"github/copilot.vim",
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {},
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>")
+		end,
+	},
 }
