0455f442711c7fc06c49b79280038b09bb6e25a5
dillonzellis <dillonzellis@gmail.com>
2024-01-09 12:20:58 -0500

    feat: add python support with Mason

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,61 +1,65 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.tsserver.setup({
-				capabilites = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilites = capabilities,
-			})
-			lspconfig.lua_ls.setup({
-				capabilites = capabilities,
-			})
+      local lspconfig = require("lspconfig")
+      lspconfig.tsserver.setup({
+        capabilites = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilites = capabilities,
+      })
+      lspconfig.lua_ls.setup({
+        capabilites = capabilities,
+      })
 
-			lspconfig["cssls"].setup({
-				capabilities = capabilities,
-			})
+      lspconfig["cssls"].setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig["tailwindcss"].setup({
-				capabilities = capabilities,
-			})
+      lspconfig["tailwindcss"].setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig["prismals"].setup({
-				capabilities = capabilities,
-			})
+      lspconfig["prismals"].setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig["graphql"].setup({
-				capabilities = capabilities,
-				filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-			})
+      lspconfig["graphql"].setup({
+        capabilities = capabilities,
+        filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+      })
 
-			lspconfig["emmet_ls"].setup({
-				capabilities = capabilities,
-				filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
-			})
+      lspconfig["emmet_ls"].setup({
+        capabilities = capabilities,
+        filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+      })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      lspconfig["pyright"].setup({
+        capabilities = capabilities,
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
