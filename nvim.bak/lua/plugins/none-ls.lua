7aa611e16b4cd6f0482cc5e119f859672dd31d7f
dillonzellis <dillonzellis@gmail.com>
2024-01-17 13:25:03 -0500

    revert: move old nvim set up to bak

---
+++ nvim.bak/lua/plugins/none-ls.lua
@@ -0,0 +1,19 @@
+return {
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.diagnostics.eslint_d,
+			},
+		})
+
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			callback = function()
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
+	end,
+}
