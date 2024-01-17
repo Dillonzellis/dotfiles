2dc7f82ba9de8461ed5546e8dfb66b7b30874802
dillonzellis <dillonzellis@gmail.com>
2024-01-17 14:58:15 -0500

    fix: remove alpha nvim

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   -- disable default plugins
   { "echasnovski/mini.pairs", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
+  { "goolord/alpha-nvim", enabled = false },
 }
