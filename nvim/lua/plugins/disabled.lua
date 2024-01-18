4a0bfe136767627d8daa5176a2766270571a20db
dillonzellis <dillonzellis@gmail.com>
2024-01-18 12:18:02 -0500

    fix: disable spectre

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "echasnovski/mini.pairs", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "goolord/alpha-nvim", enabled = false },
+  { "nvim-pack/nvim-spectre", enabled = false },
 }
