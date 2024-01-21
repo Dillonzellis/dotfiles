7641f36dfda657a710f835459b4940bfdb8468bf
dillonzellis <dillonzellis@gmail.com>
2024-01-21 00:27:59 -0500

    feat: add several colorscheme options

--- nvim/lua/plugins/colorshemes.lua
+++ nvim/lua/plugins/colorshemes.lua
@@ -1,6 +1,9 @@
 return {
   { "craftzdog/solarized-osaka.nvim" },
   { "ellisonleao/gruvbox.nvim" },
+  { "rmehri01/onenord.nvim" },
+  { "Shatur/neovim-ayu" },
+  { "gbprod/nord.nvim" },
 
   -- {
   --   "LazyVim/LazyVim",
