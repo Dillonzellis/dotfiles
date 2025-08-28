4a43cf1c39808f5b4a6af139919f561210db04ec
Dillon Ellis <31318165+Dillonzellis@users.noreply.github.com>
2025-08-28 01:29:05 -0400

    set up script create backup folder first and use ssh

--- scripts/setup-dotfiles.sh
+++ scripts/setup-dotfiles.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
-git clone --bare https://github.com/Dillonzellis/dotfiles.git $HOME/.dotfiles
+
+git clone --bare git@github.com:Dillonzellis/dotfiles.git $HOME/.dotfiles
 
 function config {
   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
 
 mkdir -p .config-backup
+
 config checkout
+
 if [ $? = 0 ]; then
   echo "Checked out config."
 else
   echo "Backing up pre-existing dot files."
-  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+  # Get list of conflicting files and back them up properly
+  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | while read file; do
+    # Create parent directory in backup location
+    mkdir -p ".config-backup/$(dirname "$file")"
+    # Move the file
+    mv "$file" ".config-backup/$file"
+    echo "Backed up: $file"
+  done
 fi
 
 config checkout
