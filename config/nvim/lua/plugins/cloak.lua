f3011753a5e9d5514c791598447679beb1064da0
dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2025-11-11 14:14:53 -0500

    add cloak

---
+++ config/nvim/lua/plugins/cloak.lua
@@ -0,0 +1,30 @@
+return {
+	"laytan/cloak.nvim",
+	opts = {
+		enabled = false,
+		cloak_character = "*",
+		highlight_group = "Comment",
+		try_all_patterns = true,
+		patterns = {
+			{
+				file_pattern = { "*" },
+				cloak_pattern = "=.+",
+				replace = "",
+			},
+			keys = {
+				{
+					"<leader>ct",
+					"<cmd>CloakToggle<CR>",
+					desc = "Toggle cloak",
+					mode = "n",
+				},
+				{
+					"<leader>cp",
+					"<cmd>CloakPreviewLine<CR>",
+					desc = "Preview cloaked line",
+					mode = "n",
+				},
+			},
+		},
+	},
+}
