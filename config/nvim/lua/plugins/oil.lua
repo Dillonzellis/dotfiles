b758d01b9f9c0e2d35ba2837601fc3045bf532b9
dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2026-04-23 13:57:20 -0400

    oil defaults to show hidden files

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -1,11 +1,16 @@
 return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  keys = {
-    { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
-  },
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  lazy = false,
+	"stevearc/oil.nvim",
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {
+		view_options = {
+			-- Show files and directories that start with "."
+			show_hidden = true,
+		},
+	},
+	keys = {
+		{ "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+	},
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	lazy = false,
 }
