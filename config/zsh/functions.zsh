a0c06c58634e745ae6e0c1e8e917595ca9c09416
dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2026-06-23 13:06:34 -0400

    move gc and grecent into standalone clis

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -10,27 +10,6 @@ wip() {
   git commit --no-verify --no-gpg-sign -m "--wip-- [skip ci]"
 }
 
-gc() {
-  local branch prefix
-
-  branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-
-  if [[ "$branch" == *"/"* ]]; then
-    prefix=$(echo "$branch" | sed 's/\/.*//')
-    git commit -m "$prefix/$*-Dillon"
-  else
-    git commit -m "$*-Dillon"
-  fi
-}
-
-grecent() {
-  git reflog --date=short |
-    grep 'checkout:' |
-    sed -E 's/.* to ([^ ]+).*/\1/' |
-    awk '!seen[$0]++' |
-    head
-}
-
 fco() {
   local b
   b="$(git branch -r --format='%(refname:short)' | fzf --prompt='checkout> ')"
