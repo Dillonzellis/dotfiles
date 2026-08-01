faf9f5944f42c58674bdea6a798c8ec05fb8cd67
dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2026-08-01 19:47:48 -0400

    clean up conform plugin and env for zshrc

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -1,55 +1,93 @@
 return {
 	"stevearc/conform.nvim",
+
 	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
+
 	keys = {
 		{
 			"<leader>cf",
 			function()
-				require("conform").format({ async = true, lsp_fallback = true })
+				require("conform").format({
+					async = true,
+				})
 			end,
 			mode = { "n", "v" },
 			desc = "Format buffer",
 		},
 	},
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "isort", "black" },
-				javascript = { "prettierd", "prettier", stop_after_first = true },
-				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-				typescript = { "prettierd", "prettier", stop_after_first = true },
-				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-				css = { "prettierd", "prettier", stop_after_first = true },
-				html = { "prettierd", "prettier", stop_after_first = true },
-				json = { "prettierd", "prettier", stop_after_first = true },
-				yaml = { "prettierd", "prettier", stop_after_first = true },
-				markdown = { "prettierd", "prettier", stop_after_first = true },
-				bash = { "shfmt" },
-				sh = { "shfmt" },
+
+	opts = {
+		-- Defaults used by manual formatting and format-on-save
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
+
+		formatters_by_ft = {
+			lua = { "stylua" },
+
+			-- These run sequentially
+			python = { "isort", "black" },
+
+			-- Use prettierd first, then prettier if prettierd is unavailable
+			javascript = {
+				"prettierd",
+				"prettier",
+				stop_after_first = true,
 			},
-			format_on_save = function()
-				if vim.g.disable_autoformat then
-					return
-				end
-				return {
-					timeout_ms = 3000,
-					lsp_fallback = true,
-				}
-			end,
-			formatters = {
-				prettier = {
-					options = {
-						ft_parsers = {
-							javascript = "babel",
-							javascriptreact = "babel",
-							typescript = "typescript",
-							typescriptreact = "tsx",
-						},
-					},
-				},
+			javascriptreact = {
+				"prettierd",
+				"prettier",
+				stop_after_first = true,
 			},
-		})
-	end,
+			typescript = {
+				"prettierd",
+				"prettier",
+				stop_after_first = true,
+			},
+			typescriptreact = {
+				"prettierd",
+				"prettier",
+				stop_after_first = true,
+			},
+			css = {
+				"prettierd",
+				"prettier",
+				stop_after_first = true,
+			},
+			html = {
+				"prettierd",
+				"prettier",
+				stop_after_first = true,
+			},
+			json = {
+				"prettierd",
+				"prettier",
+				stop_after_first = true,
+			},
+			yaml = {
+				"prettierd",
+				"prettier",
+				stop_after_first = true,
+			},
+			markdown = {
+				"prettierd",
+				"prettier",
+				stop_after_first = true,
+			},
+
+			bash = { "shfmt" },
+			sh = { "shfmt" },
+		},
+
+		format_on_save = function(bufnr)
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+
+			return {
+				timeout_ms = 3000,
+			}
+		end,
+	},
 }
--- config/zsh/env.zsh
+++ config/zsh/env.zsh
@@ -1,19 +1,7 @@
 export EDITOR="nvim"
 
 export PATH="$HOME/.local/bin:$PATH"
-export PATH="$HOME/Library/Python/3.9/bin:$PATH"
-
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home"
-
 export ANDROID_HOME="$HOME/Library/Android/sdk"
 export PATH="$PATH:$ANDROID_HOME/emulator"
 export PATH="$PATH:$ANDROID_HOME/platform-tools"
-
-export PATH="$PATH:/Users/dillon.ellis/.spicetify"
-export PATH="/Users/dillon.ellis/.opencode/bin:$PATH"
-
-export PNPM_HOME="/Users/dillon.ellis/Library/pnpm"
-case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
-esac
