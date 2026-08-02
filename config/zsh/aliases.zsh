45175b01a25a937739bc1dd79828322966ecc1da
dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2026-08-02 12:35:44 -0400

    remove dir aliases

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -15,14 +15,8 @@ alias nld="nightlight temp 70"
 alias nln="nightlight temp 100"
 alias nls="nightlight status && nightlight temp"
 
-alias ajc="cd ~/wks/arc-fusion-ajc"
-alias dawg="cd ~/wks/arc-fusion-dawgnation/"
-alias pl="cd ~/wks/ajc-payload/"
 alias dl="cd ~/wks/dl"
 alias desk="cd ~/Desktop"
-
-alias ns="npx fusion start"
-alias ajcdev="npx cross-env NODE_OPTIONS=--no-deprecation next dev --turbo"
 
 alias dsu="~/.local/bin/dsu-aerospace.sh"
 
